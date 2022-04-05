ce7aa6e77a256edccac67e959b308c09792ed609
wegorobotics <wegorobotics@gmail.com>
2022-04-05 11:58:25 -0500

    changed variable names to have a,b,c instead of l,r,m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Kiwi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Kiwi.java
@@ -9,57 +9,57 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 //@Disabled
 public class TeleOp_Kiwi extends OpMode {
 
-    DcMotor lWheel;
-    DcMotor rWheel;
-    DcMotor mWheel;
+    DcMotor aWheel;
+    DcMotor bWheel;
+    DcMotor cWheel;
 
     @Override
     public void init() {
-        lWheel = hardwareMap.get(DcMotor.class, "lWheel");
-        rWheel = hardwareMap.get(DcMotor.class, "rWheel");
-        mWheel = hardwareMap.get(DcMotor.class, "mWheel");
+        aWheel = hardwareMap.get(DcMotor.class, "aWheel");
+        bWheel = hardwareMap.get(DcMotor.class, "bWheel");
+        cWheel = hardwareMap.get(DcMotor.class, "cWheel");
 
-        lWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        mWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        aWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        cWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        lWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        mWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        aWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        cWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
-        double lPower;
-        double rPower;
-        double mPower;
+        double aPower;
+        double bPower;
+        double cPower;
 
         double leftX = gamepad1.left_stick_x;
         double leftY = gamepad1.left_stick_y;
         double rightX = gamepad1.right_stick_x;
 
-        lPower = -leftX;
-        rPower = leftX / 2;
-        mPower = leftX / 2;
+        aPower = -leftX;
+        bPower = leftX / 2;
+        cPower = leftX / 2;
 
         leftY = leftY * (Math.sqrt(3) / 2);
-        rPower += -(leftY);
-        mPower += leftY;
+        bPower += -(leftY);
+        cPower += leftY;
 
-        lPower += rightX;
-        rPower += rightX;
-        mPower += rightX;
+        aPower += rightX;
+        bPower += rightX;
+        cPower += rightX;
 
-        if (Math.abs(lPower) > 1 || Math.abs(rPower) > 1 || Math.abs(mPower) > 1) {
-            double maxPower = findAbsoluteMax(lPower, rPower, mPower);
-            lPower /= maxPower;
-            rPower /= maxPower;
-            mPower /= maxPower;
+        if (Math.abs(aPower) > 1 || Math.abs(bPower) > 1 || Math.abs(cPower) > 1) {
+            double maxPower = findAbsoluteMax(aPower, bPower, cPower);
+            aPower /= maxPower;
+            bPower /= maxPower;
+            cPower /= maxPower;
         }
 
-        lWheel.setPower(lPower);
-        rWheel.setPower(rPower);
-        mWheel.setPower(mPower);
+        aWheel.setPower(aPower);
+        bWheel.setPower(bPower);
+        cWheel.setPower(cPower);
         }
 
     double findAbsoluteMax(double p1, double p2, double p3) {
